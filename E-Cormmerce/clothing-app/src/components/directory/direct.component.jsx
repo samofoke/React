e8762f592e44b5cd0519ec77f297f08f31bf4eef
samofoke <sabatamofokeng3@gmail.com>
2020-03-11 19:39:25 +0200

    build1

---
+++ E-Cormmerce/clothing-app/src/components/directory/direct.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import React from 'react';
+
+class Direct extends React.Component {
+    constructor() {
+        super();
+
+        this.state {
+            sections: [{
+                
+            }]
+        }
+    }
+}
