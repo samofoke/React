22bf0c40b3beb6fec6d2f39e2ffde7735ccbac16
samofoke <samofoke@student.wethinkcode.co.za>
2020-03-22 06:32:52 +0200

    build4

--- E-Cormmerce/clothing-app/src/components/directory/direct.component.jsx
+++ E-Cormmerce/clothing-app/src/components/directory/direct.component.jsx
@@ -13,7 +13,7 @@ class Direct extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
@@ -48,8 +48,8 @@ class Direct extends React.Component {
         return (
             <div className='direct-menu'>
                 {
-                    this.state.sections.map(({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    this.state.sections.map(({id, ...otherSectionProps}) => (
+                        <MenuItem key={id} {...otherSectionProps} />
                     ))
                 }
             </div>            
